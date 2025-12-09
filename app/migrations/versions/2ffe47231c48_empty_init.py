65125fbf6be4447259dd769cf62f80f43509a231
Your Name <youremail@example.com>
2025-12-09 18:12:38 +0300

    finished assignment

---
+++ app/migrations/versions/2ffe47231c48_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 2ffe47231c48
+Revises: 
+Create Date: 2025-12-09 18:03:29.564536
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2ffe47231c48'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ app/migrations/versions/46d47fe9cd81_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 46d47fe9cd81
+Revises: 2ffe47231c48
+Create Date: 2025-12-09 18:04:07.430774
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '46d47fe9cd81'
+down_revision = '2ffe47231c48'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
